8e332877ba5dccc5d0c8d68779f6ec1f0b008001
iotboy <naga.kona@outlook.com>
2016-02-02 01:02:30 +0000

    config and index

--- index.js
+++ index.js
@@ -61,79 +61,76 @@ function zigbeeDevice(json){
             if (seen[device.IEEEAddress]) {
               return;
             }
-            seen[device.IEEEAddress] = true;
             //console.log("Living Room Device: " + self.config.IEEEHueLivingRoom);
-            if(device.IEEEAddress == self.config.IEEEHueLivingRoom) {
-              console.log('Found', device.toString());
-              device.on('endpoint', function(endpoint) {
-                console.log('Endpoint: ', endpoint.toString());
-                console.log('Endpoint ID: ',endpoint.endpointId);
-                endpoint.inClusters().then(function(clusters) {
-                  clusters.forEach(function(cluster) {
-                    console.log('Cluster: ', cluster.toString());
-                    if(cluster.name == 'Level Control') {
-                      cluster.attributes.CurrentLevel.read().then(function(level) {
-                        console.log('Current level', level);
-                      });
-                    }
-                    //seen[device.IEEEAddress] = true;
-                  });
-                  if(( device.IEEEAddress == self.config.IEEEHueLivingRoom) && (endpoint.endpointId == self.config.HueEndPoint)) {
-                    console.log('Set Living Room Levels variable');
-                    self.levelControlCluster_HueLivingRoom = _.findWhere(clusters, {name: 'Level Control'});
-                    self.colorCluster_HueLivingRoom = _.findWhere(clusters, {name: 'Color Control'});
-                    self.onOffCluster_HueLivingRoom = _.findWhere(clusters, {name: 'On/Off'});
-                    console.log('Set Living Room Color Variable');
-                    exports.colorCluster_HueLivingRoom = self.colorCluster_HueLivingRoom;
-                    exports.levelControlCluster_HueLivingRoom = self.levelControlCluster_HueLivingRoom;
-                    if(self.colorCluster_HueLivingRoom){
-                      setColor(self.colorCluster_HueLivingRoom, '#FF00FF');
-                      console.log('Variable NOT NULL Call setColor');
-                    }
-                  }
+            console.log('Found', device.toString());
+            device.on('endpoint', function(endpoint) {
+              console.log('Endpoint: ', endpoint.toString());
+              console.log('Endpoint ID: ',endpoint.endpointId);
+              endpoint.inClusters().then(function(clusters) {
+                clusters.forEach(function(cluster) {
+                  console.log('Cluster: ', cluster.toString());
+                  seen[device.IEEEAddress] = true;
+                  // if(cluster.name == 'Level Control') {
+                  //   cluster.attributes.CurrentLevel.read().then(function(level) {
+                  //     console.log('Current level', level);
+                  //   });
+                  // }
                 });
+                if(( device.IEEEAddress == self.config.IEEEHueLivingRoom) && (endpoint.endpointId == self.config.HueEndPoint)) {
+                  console.log('Set Living Room Levels variable');
+                  exports.levelControlCluster_HueLivingRoom = self.levelControlCluster_HueLivingRoom = _.findWhere(clusters, {name: 'Level Control'});
+                  exports.colorCluster_HueLivingRoom = self.colorCluster_HueLivingRoom = _.findWhere(clusters, {name: 'Color Control'});
+                  exports.onOffCluster_HueLivingRoom = self.onOffCluster_HueLivingRoom = _.findWhere(clusters, {name: 'On/Off'});
+                  console.log('Set Living Room Color Variable');
+
+                  if(self.colorCluster_HueLivingRoom){
+                    setColor(self.colorCluster_HueLivingRoom, '#FF00FF');
+                    console.log('Variable NOT NULL Call setColor');
+                  }
+                }
               });
-            }
-            device.findActiveEndpoints();
-            device.findEndpoints(0x0104, [0x0500], [0x0500]); // HA IAS Zones.
-          });
+            });
+          }
+          device.findActiveEndpoints();
+          device.findEndpoints(0x0104, [0x0500], [0x0500]); // HA IAS Zones.
         });
-      }, 5000);
-    });
-  };
+      });
+    }, 5000);
+  });
+};
 
-  zigbeeDevice.prototype.setPower = function(value) {
-    if(!self.onOffCluster_HueLivingRoom)
-    return;
-    console.log('In set power: ', value);
-    if(value)
-    self.onOffCluster_HueLivingRoom.commands.On();
-    else
-    self.onOffCluster_HueLivingRoom.commands.Off();
-  };
+zigbeeDevice.prototype.setPower = function(value) {
+  if(!self.onOffCluster_HueLivingRoom)
+  return;
+  console.log('In set power: ', value);
+  if(value)
+  self.onOffCluster_HueLivingRoom.commands.On();
+  else
+  self.onOffCluster_HueLivingRoom.commands.Off();
+};
 
-  zigbeeDevice.prototype.changeBrightness = function(value) {
-    if(!self.levelControlCluster_HueLivingRoom)
-    return;
-    var payload = new concentrate();
-    payload.uint8(value); // Level
-    payload.uint16le(0); // Transition duration (1/10th seconds)
-    self.levelControlCluster_HueLivingRoom.commands['Move to Level (with On/Off)'](payload.result()).done();
-  };
+zigbeeDevice.prototype.changeBrightness = function(value) {
+  if(!self.levelControlCluster_HueLivingRoom)
+  return;
+  var payload = new concentrate();
+  payload.uint8(value); // Level
+  payload.uint16le(0); // Transition duration (1/10th seconds)
+  self.levelControlCluster_HueLivingRoom.commands['Move to Level (with On/Off)'](payload.result()).done();
+};
 
-  zigbeeDevice.prototype.setColor = function(cluster, hex) {
-    console.log('In setColor');
-    if(!cluster) return;
-    if(!self.colorCluster_HueLivingRoom)
-    return;
-    console.log('Set Color: ' + hex);
-    var color = colorspaces.make_color('hex', hex).as('CIExyY');
-    var payload = new concentrate();
-    payload.uint16le(Math.floor(color[0] * 0xFFFF)); // Color X
-    payload.uint16le(Math.floor(color[1] * 0xFFFF)); // Color Y
-    payload.uint16le(0); // Transition duration (1/10th seconds)
-    return cluster.commands['Move to Color'](payload.result());
-  };
+zigbeeDevice.prototype.setColor = function(cluster, hex) {
+  console.log('In setColor');
+  if(!cluster) return;
+  if(!self.colorCluster_HueLivingRoom)
+  return;
+  console.log('Set Color: ' + hex);
+  var color = colorspaces.make_color('hex', hex).as('CIExyY');
+  var payload = new concentrate();
+  payload.uint16le(Math.floor(color[0] * 0xFFFF)); // Color X
+  payload.uint16le(Math.floor(color[1] * 0xFFFF)); // Color Y
+  payload.uint16le(0); // Transition duration (1/10th seconds)
+  return cluster.commands['Move to Color'](payload.result());
+};
 
 
 }
